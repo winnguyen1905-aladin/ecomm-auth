b524e6d9584ae469b7b0b40bff348eccaf6a3679
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2023-12-19 15:59:18 +0700

    update authservice

--- src/main/java/com/winnguyen1905/gateway/core/service/AuthService.java
+++ src/main/java/com/winnguyen1905/gateway/core/service/AuthService.java
@@ -3,7 +3,6 @@ package com.winnguyen1905.gateway.core.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,20 +13,21 @@ import com.winnguyen1905.gateway.core.model.CustomUserDetails;
 import com.winnguyen1905.gateway.core.model.request.LoginRequest;
 import com.winnguyen1905.gateway.core.model.request.RegisterRequest;
 import com.winnguyen1905.gateway.core.model.response.AuthResponse;
-import com.winnguyen1905.gateway.persistance.entity.ERole;
+import com.winnguyen1905.gateway.exception.ResourceAlreadyExistsException;
+import com.winnguyen1905.gateway.exception.ResourceNotFoundException;
 import com.winnguyen1905.gateway.persistance.entity.EUserCredentials;
 import com.winnguyen1905.gateway.persistance.repository.RoleRepository;
 import com.winnguyen1905.gateway.persistance.repository.UserRepository;
 import com.winnguyen1905.gateway.util.JwtUtils;
 import com.winnguyen1905.gateway.util.TokenPair;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class AuthService implements IAuthService {
 
@@ -42,61 +42,77 @@ public class AuthService implements IAuthService {
 
   @Override
   public Mono<AuthResponse> handleLogin(LoginRequest loginRequest) {
-    Mono<Authentication> authenResult = this.reactiveAuthenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
-            loginRequest.getPassword()));
-
-    return authenResult
-        .publishOn(Schedulers.boundedElastic())
+    return this.reactiveAuthenticationManager
+        .authenticate(new UsernamePasswordAuthenticationToken(
+            loginRequest.getUsername(),
+            loginRequest.getPassword()))
         .map(authentication -> {
-          TokenPair tokenPair = this.jwtUtils.createTokenPair((CustomUserDetails) authentication.getPrincipal());
-          handleUpdateUsersRefreshToken(loginRequest.getUsername(), tokenPair.getRefreshToken());
-          return this.authenResponseConverter.toAuthenResponse(this.mapper.map(authentication, EUserCredentials.class),
-              tokenPair);
+          if (!(authentication.getPrincipal() instanceof CustomUserDetails customUserDetails)) {
+            throw new ClassCastException(
+                "Unexpected principal type: " + authentication.getPrincipal().getClass());
+          }
+          return customUserDetails;
+        })
+        .map(userDetails -> {
+          TokenPair tokenPair = jwtUtils.createTokenPair(userDetails);
+          EUserCredentials userCredentials = mapper.map(userDetails, EUserCredentials.class);
+
+          handleUpdateUsersRefreshToken(loginRequest.getUsername(), tokenPair.getRefreshToken())
+              .subscribeOn(Schedulers.boundedElastic()).subscribe();
+          return this.authenResponseConverter.toAuthenResponse(userCredentials, tokenPair);
         });
   }
 
   @Override
-  public AuthResponse handleRegister(RegisterRequest registerRequest) {
-    this.userRepository
-        .findUserByUsername(registerRequest.getUsername())
-        .ifPresent(user -> {
-          throw new RuntimeException("User already exists");
+  public Mono<AuthResponse> handleRegister(RegisterRequest registerRequest) {
+    EUserCredentials eUserCredentials = this.userRepository.findUserByUsername(registerRequest.getUsername())
+        .orElse(null);
+    if (eUserCredentials != null)
+      throw new ResourceAlreadyExistsException("Username already exists");
+    return Mono.just(userConverter.toUserEntity(registerRequest))
+        .flatMap(user -> {
+          user.setPassword(passwordEncoder.encode(user.getPassword()));
+          return Mono.fromCallable(() -> roleRepository.findByCode("admin"))
+              .flatMap(optionalRole -> Mono.justOrEmpty(optionalRole))
+              .switchIfEmpty(Mono.error(new ResourceNotFoundException("Admin role not found")))
+              .map(role -> {
+                user.setRole(role);
+                return this.userRepository.save(user);
+              })
+              .map(savedUser -> this.authenResponseConverter.toAuthenResponse(savedUser, null));
         });
-    EUserCredentials customer = this.userConverter.toUserEntity(registerRequest);
-    ERole customerRole = this.mapper.map(this.roleRepository.findByCode("admin"), ERole.class);
-    customer.setPassword(this.passwordEncoder.encode(customer.getPassword()));
-    customer.setRole(customerRole);
-    return this.authenResponseConverter.toAuthenResponse(customer, null);
+  }
+
+  private Mono<Void> handleUpdateUsersRefreshToken(String username, String refreshToken) {
+    return Mono.fromCallable(() -> this.userRepository.findUserByUsername(username))
+        .flatMap(optionalUser -> Mono.justOrEmpty(optionalUser))
+        .switchIfEmpty(Mono.error(new UsernameNotFoundException("Not found user by username " + username)))
+        .doOnNext(user -> user.setRefreshToken(refreshToken))
+        .flatMap(user -> Mono.fromCallable(() -> this.userRepository.save(user)))
+        .then();
   }
 
   @Override
-  public Mono<Void> handleUpdateUsersRefreshToken(String username, String refreshToken) {
-    EUserCredentials user = this.userRepository.findUserByUsername(username)
-        .orElseThrow(() -> new UsernameNotFoundException(
-            "Not found user with username " + username));
-    user.setRefreshToken(refreshToken);
-    this.userRepository.save(user);
-    return Mono.empty();
-  }
-
-  @Override
-  public AuthResponse handleGetAuthenResponseByUsernameAndRefreshToken(String username, String refreshToken) {
-    EUserCredentials user = this.userRepository.findByUsernameAndRefreshToken(username, refreshToken)
-        .orElseThrow(() -> new UsernameNotFoundException("Not found user with refresh token and username " + username));
-    TokenPair tokenPair = this.jwtUtils.createTokenPair(this.mapper.map(user, CustomUserDetails.class));
-    handleUpdateUsersRefreshToken(username, tokenPair.getRefreshToken());
-    return this.authenResponseConverter.toAuthenResponse(user, tokenPair);
+  public Mono<AuthResponse> handleRefreshToken(String username, String refreshToken) {
+    return Mono.fromCallable(() -> this.userRepository.findByUsernameAndRefreshToken(username, refreshToken))
+        .flatMap(optionalUser -> Mono.justOrEmpty(optionalUser))
+        .switchIfEmpty(Mono.error(new UsernameNotFoundException("Not found user by username " + username)))
+        .map(user -> {
+          TokenPair tokenPair = this.jwtUtils.createTokenPair(this.mapper.map(user, CustomUserDetails.class));
+          handleUpdateUsersRefreshToken(username, tokenPair.getRefreshToken())
+              .subscribeOn(Schedulers.boundedElastic())
+              .subscribe();
+          return this.authenResponseConverter.toAuthenResponse(user, tokenPair);
+        });
   }
 
   @Override
   public Mono<Void> handleLogout(String username) {
-    EUserCredentials user = this.userRepository.findUserByUsername(username)
-        .orElseThrow(() -> new UsernameNotFoundException(
-            "Not found user with username " + username));
-    user.setRefreshToken(null);
-    this.userRepository.save(user);
-    return Mono.empty();
+    return Mono.fromCallable(() -> this.userRepository.findUserByUsername(username))
+        .flatMap(optionalUser -> Mono.justOrEmpty(optionalUser))
+        .switchIfEmpty(Mono.error(new UsernameNotFoundException("Not found user by username " + username)))
+        .doOnNext(user -> user.setRefreshToken(null))
+        .flatMap(user -> Mono.fromCallable(() -> this.userRepository.save(user)).then());
   }
 
 }
