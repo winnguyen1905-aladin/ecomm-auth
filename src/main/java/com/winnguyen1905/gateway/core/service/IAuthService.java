19aac8593da417058dd676a3723f3b621da27900
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2023-12-19 13:58:08 +0700

    update iauthservice

--- src/main/java/com/winnguyen1905/gateway/core/service/IAuthService.java
+++ src/main/java/com/winnguyen1905/gateway/core/service/IAuthService.java
@@ -7,13 +7,11 @@ import com.winnguyen1905.gateway.core.model.response.AuthResponse;
 import reactor.core.publisher.Mono;
 
 public interface IAuthService {
+  public Mono<Void> handleLogout(String username);
+
   public Mono<AuthResponse> handleLogin(LoginRequest loginRequest);
 
-  public AuthResponse handleRegister(RegisterRequest registerRequest);
+  public Mono<AuthResponse> handleRegister(RegisterRequest registerRequest);
 
-  public AuthResponse handleGetAuthenResponseByUsernameAndRefreshToken(String username, String refreshToken);
-
-  public Mono<Void> handleUpdateUsersRefreshToken(String username, String refreshToken);
-
-  public Mono<Void> handleLogout(String username);
+  public Mono<AuthResponse> handleRefreshToken(String username, String refreshToken);
 }
