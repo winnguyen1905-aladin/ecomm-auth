848ec8f741dd6639799747d043a4ca03f5d326b6
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2024-03-04 21:06:19 +0700

    reactiveUserDetailsService & reactiveAuthenticationManager config lamda bean

--- src/main/java/com/winnguyen1905/gateway/config/AuthenticationConfig.java
+++ src/main/java/com/winnguyen1905/gateway/config/AuthenticationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.winnguyen1905.gateway.core.model.CustomUserDetails;
@@ -20,20 +19,18 @@ public class AuthenticationConfig {
   ReactiveAuthenticationManager reactiveAuthenticationManager(
       ReactiveUserDetailsService userDetailsService,
       PasswordEncoder passwordEncoder) {
-    var authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
-    authenticationManager.setPasswordEncoder(passwordEncoder);
-    
-    // Optional configurations
-    authenticationManager.setUserDetailsPasswordService(null); // For password upgrade
-    authenticationManager.setPostAuthenticationChecks(user -> Mono.just(user)); // Additional checks after authentication
-    
-    return authenticationManager;
+    var authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+    authManager.setPasswordEncoder(passwordEncoder);
+    return authManager;
   }
 
   @Bean("reactiveUserDetailsService")
   ReactiveUserDetailsService reactiveUserDetailsService(ModelMapper mapper, UserRepository userRepository) {
-    return username -> userRepository.findUserByUsername(username)
-        .switchIfEmpty(Mono.error(new UsernameNotFoundException("Not found user by username " + username)))
-        .map(user -> mapper.map(user, CustomUserDetails.class));
+    return username -> Mono.fromCallable(() -> userRepository.findUserByUsername(username))
+        .flatMap(optionalUser ->
+            Mono.justOrEmpty(optionalUser))
+        // .switchIfEmpty(Mono.error(new UsernameNotFoundException("Not found user by username " + username)))
+        .map(user ->
+            mapper.map(user, CustomUserDetails.class));
   }
 }
